7b806a3256fd49d915c24afb8af507459487ade4
Amar-06 <amarcv139@gmail.com>
2026-06-17 21:55:52 +0530

    Added Bootstrap styling and custom CSS

--- food/urls.py
+++ food/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name='food'
 urlpatterns=[path('amar',views.my_Intro,name='my_Intro'),
              path('',views.item,name='item'),
              path('menu/',views.Food_List,name='Food_List'),
